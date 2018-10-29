91ea4302b6fe58bed23c9eb47af504b9f7f71f6a
fix <fix@mgecfast.(none)>
2018-10-29 13:31:54 +0300

    Fix error in block. Add timestamp property

--- pypcappy/blocks/enhancedpacket.py
+++ pypcappy/blocks/enhancedpacket.py
@@ -14,12 +14,18 @@ class EnhancedPacketBlock(AbstractBlock):
 
     @property
     def capture_len(self):
-        return int.from_bytes(self.data[20:25], byteorder=self.byteorder, signed=False)
+        return int.from_bytes(self.data[12:25], byteorder=self.byteorder, signed=False)
 
     @property
     def original_len(self):
-        return int.from_bytes(self.data[20:25], byteorder=self.byteorder, signed=False) 
+        return int.from_bytes(self.data[12:25], byteorder=self.byteorder, signed=False)
 
     @property
     def packet(self):
-        return Packet(self.byteorder, self.data[28:28 + 1 + self.capture_len])
+        return Packet(self.byteorder, self.data[20:20 + 1 + self.capture_len])
+
+    @property
+    def timestamp(self):
+        hight = int.from_bytes(self.data[4:8], byteorder=self.byteorder, signed=False)
+        low = int.from_bytes(self.data[8:12], byteorder=self.byteorder, signed=False)
+        return (hight << 32) | low
